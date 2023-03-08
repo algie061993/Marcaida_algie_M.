54f8222afc7e7dd932479c39df45b25a5ef0afb9
unknown <algie061993@gmail.com>
2023-03-08 15:35:19 +0800

    testing

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "killerpatola";
+$dbname = "web_db";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, firstname, lastname FROM employee_list";
+$result = $conn->query($sql);
+
+if ($result-> mysqli_result::$num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
